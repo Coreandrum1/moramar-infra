cbc3f58971f13070461b612fd285880854aefa52
Coreandrum1 <core_andrum769@hotmail.com>
2024-10-03 23:35:08 -0700

    test

--- api/index.ts
+++ api/index.ts
@@ -1,16 +1,3 @@
-import express, { Request, Response } from "express";
-
-const app = express();
-const port = 4000;
-
-app.get("/", (req: Request, res: Response) => {
-  res.send("Hello, TypeScript with Express!");
-});
-
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
-});
-
-console.log("Hello, TypeScript with Express!");
+import app from "../src/index";
 
 export default app;
