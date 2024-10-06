ffe0ff53534ae4e0755db444e736780d14b23e94
Coreandrum1 <core_andrum769@hotmail.com>
2024-10-06 00:21:40 -0700

    added options

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -5,15 +5,12 @@ export const corsOptions = {
     origin: string | undefined,
     callback: (err: Error | null, allow?: boolean) => void
   ) {
-    console.log("Incoming origin:", origin);
-    console.log("Allowed origins:", allowedOriginsArray);
-
     if (!origin || allowedOriginsArray.indexOf(origin) !== -1) {
       callback(null, true);
     } else {
-      console.log("Blocked by CORS:", origin);
       callback(new Error("Not allowed by CORS"));
     }
   },
+  optionsSuccessStatus: 200,
   credentials: true, // Enable sending cookies if needed
 };
